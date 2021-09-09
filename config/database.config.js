e234a915147edeaa1ca88ec3ba877a5174cad5aa
Juansecod <juan.sebastianri28@gmail.com>
2021-09-08 22:33:24 -0500

    Primera version de la API para usar y realizar peticiones desde el frontend terminada

---
+++ config/database.config.js
@@ -0,0 +1,17 @@
+const { Client } = require('pg');
+require('dotenv').config({path: '../.env' || './.env'});
+
+const clientGenerator = ({ user, host, database, password, port }) => {
+    return new Client({user, host, database, password, port});
+};
+
+const client = clientGenerator(process.env);
+
+client.connect()
+    .then(() => {
+        console.log('Conectado a la base de datos: ' + process.env.database);
+    }).catch(err => {
+        console.error('Error de conexion: ', err.message);
+    });
+
+module.exports = client;
---
+++ config/server.config.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = '/api/v1';
+const port = 3010;
+
+app.use(cors());
+app.use(express.json());
+
+const prediosRoutes = require('../routers/predios.routes');
+const propietariosRoutes = require('../routers/propietarios.routes');
+const construccionesRoutes = require('../routers/construcciones.routes');
+const terrenosRoutes = require('../routers/terrenos.routes');
+
+app.use(`${path}/predios`, prediosRoutes);
+app.use(`${path}/predios/propietarios`, propietariosRoutes);
+app.use(`${path}/predios/construcciones`, construccionesRoutes);
+app.use(`${path}/predios/terrenos`, terrenosRoutes);
+
+app.listen(port, () => console.log(`Servidor en el puerto ${port}. Para acceder use la url: localhost:${port}`));
+
+module.exports = app;
